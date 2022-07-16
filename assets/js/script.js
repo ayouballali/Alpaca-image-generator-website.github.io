9dd5c6e880d11b199bd1d85d966980a6eaee47f3
ayouballali <ayoubralali@gmail.com>
2022-07-16 21:47:11 +0100

    continue styling and add events to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+/* ------------- show the convenable style buttons--------------  */
+const accessorize__buttons = document.querySelectorAll('.accessorize__btn'),
+      styles = document.querySelectorAll('.style') ;
+      
+accessorize__buttons.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+
+        /*============styling the button================ */
+        accessorize__buttons.forEach((btn1)=>{
+                  if (btn.id == btn1.id)
+                        btn1.classList.add('accessorize__active');
+                  else
+                        btn1.classList.remove('accessorize__active');
+        })
+        /* ==============showing the style==========*/ 
+            styles.forEach((style)=>{
+                  if (style.classList.contains(btn.id))
+                        style.classList.add('style__active');
+                  else
+                        style.classList.remove('style__active');
+                
+        })
+    })
+})
+
+/*==================styling the style buttons========================= */
+const style__buttons = document.querySelectorAll('.style__button');
+
+style__buttons.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+
+        /*============styling the button================ */
+        style__buttons.forEach((btn1)=>{
+                  if (btn == btn1)
+                        btn1.classList.add('btn__style__active');
+                  else
+                        btn1.classList.remove('btn__style__active');
+        })
+        /* ==============showing the styles in the image ==========*/ 
+           
+    })
+})

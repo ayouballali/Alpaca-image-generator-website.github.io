73bb074bb2a62486ecbff0694cfd35f980eedc7e
ayouballali <ayoubralali@gmail.com>
2022-07-25 12:02:56 +0100

    comit

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,5 +51,40 @@ style__buttons.forEach((btn) => {
   });
 });
 
+/*================== randomly  ========================= */
 
-/*================== randomly  ========================= */ 
+let randomButton = document.getElementById("random");
+
+let haires = document.querySelectorAll(".hair").childElement.contttent;
+let eyes = document.querySelectorAll(".eyes").childElement.innerHTML;
+let ears = document.querySelectorAll(".ears").childElement.innerHTML;
+let legs = document.querySelectorAll(".legs").childElementinnerHTML;
+let accessoires =
+  document.querySelectorAll(".accessoires").childElement.innerHTML;
+let backgrounds =
+  document.querySelectorAll(".backgrounds").childElement.innerHTML;
+let mouth = document.querySelectorAll(".mouth").childElement.innerHTML;
+
+randomButton.addEventListener("click", () => {
+  haires.random();
+  eyes.random();
+});
+
+/*================== function that get a value than put the convenant image and make it active    ========================= */
+
+function putThisImage(element, accessoire) {
+  let imagetrigredElemt = document.getElementById(accessoire);
+
+  imagetrigred.setAttribute(
+    "src",
+    "assets/img/" + accessoire + "/" + element + ".png"
+  );
+
+  /*============styling the button================ */
+  style__buttons.forEach((btn2) => {
+    if (accessoire == btn2.parentElement.classList[0]) {
+      if (accessoire == btn2) btn2.classList.add("btn__style__active");
+      else btn2.classList.remove("btn__style__active");
+    }
+  });
+}
